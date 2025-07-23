38fef5d83a049ac0cf6faaac3d0b6d8380e18e05
GhandourGh <gghandour03@gmail.com>
2025-07-23 16:25:43 +0300

    Update: dual deploy vite config for Netlify and GitHub Pages

--- bthub/vite.config.js
+++ bthub/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const isGH = process.env.DEPLOY_TARGET === 'GH';
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/btengineering/',
+  base: isGH ? '/btengineering/' : '/',
   plugins: [react(), tailwindcss()],
 })
